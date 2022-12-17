3e7586a9ccf245c75f31e5a6f62e58877dd7efe0
Eric Lyons <eric.lyonford@gmail.com>
2022-12-17 01:05:31 -0600

    Setting up with thymeleaf, validation and web.

---
+++ src/main/java/com/example/jsonmvc/JsonmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsonmvc/JsonmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
